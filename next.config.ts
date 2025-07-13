584cf824c3d16ec7b031e18e1b4e1aeb0965fdb2
Mike Coates <mikecoates73@gmail.com>
2025-07-13 15:21:59 +0700

    Fixed errors in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'eoimages.gsfc.nasa.gov',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
